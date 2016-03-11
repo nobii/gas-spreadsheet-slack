aa1759a9015043c28558a6d4edc55f442cf94453
FumikoIshizawa <p.krrr.731@gmail.com>
2016-03-11 18:17:39 +0900

    Modify menu bar to use easily

--- deadline-notification.js
+++ deadline-notification.js
@@ -8,8 +8,11 @@ var DAY_MSECONDS = 1000 * 60 * 60 * 24;
 
 function onOpen() {
   SpreadsheetApp.getUi()
-    .createMenu("Slackの設定")
-    .addItem("Slack API Tokenの登録", "setAPIToken")
+    .createMenu("Slackに投稿")
+    .addItem("今すぐ実行", "showExecutePrompt")
+    .addSeparator()
+    .addSubMenu(SpreadsheetApp.getUi().createMenu('設定')
+      .addItem("Slack API Tokenの登録", "setAPIToken"))
     .addToUi();
 }
 
@@ -35,97 +38,42 @@ function saveToken(str) {
 }
 
 
-function postNotificationMessage() {
-  var now = new Date();
-  var workdays = loadWorkdays(now);
-  if (!workdays[0]) {
+function showExecutePrompt() {
+  var output = HtmlService.createHtmlOutputFromFile('prompt')
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .setWidth(400)
+    .setHeight(200);
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  ss.show(output);
+}
+
+
+function getProjectList() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var values = sheet.getSheetValues(2, 1, sheet.getLastRow(), 1);
+  return values;
+}
+
+
+function postSingleNotificationMessage(row) {
+  var notification = new Notification();
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var values = sheet.getSheetValues(row, 1, 1, sheet.getLastColumn());
+  notification.postMessage(values[0]);
+}
+
+
+function postAllNotificationMessage() {
+  var notification = new Notification();
+  if (notification.isHoliday()) {
     return;
   }
-  
-  var slackMessage = new SlackMessage();
+
   var sheet = SpreadsheetApp.getActiveSheet();
   var values = sheet.getDataRange().getValues();
   var maxRow = sheet.getLastRow();
-  var sheetUrl = SpreadsheetApp.getActiveSpreadsheet().getUrl();
   
   for (var r = 1; r < maxRow; r++) {
-    var title = values[r][COLUMN_TITLE];
-    var channel = values[r][COLUMN_CHANNEL];
-    var launchDate = values[r][COLUMN_LAUNCH_DATE];
-    var icon = values[r][COLUMN_ICON];
-    
-    var message;
-    var prevDate = new Date();
-    prevDate.setDate(prevDate.getDate() - 1);
-    if (launchDate < prevDate) {
-      message = "このプロジェクトは終了しました！<" + sheetUrl + "|ここ>から削除してください";
-    } else {
-      var leftDays = Math.ceil((launchDate - now) / DAY_MSECONDS);
-      var leftWorkDays = countWorkday(leftDays, workdays);
-      message = (launchDate.getMonth() + 1) + "月" + launchDate.getDate() + "日まであと" + leftWorkDays + "営業日です";
-    }
-  
-    slackMessage.postMessage(title, channel, message, icon);
+    notification.postMessage(values[r]);
   }
-}
-
-
-var SlackMessage = (function() {
-  function SlackMessage() {
-    var prop = PropertiesService.getScriptProperties().getProperties();
-    this.slackApp = SlackApp.create(prop.token);
-    this.channels = this.slackApp.channelsList().channels;
-  }
-  
-  SlackMessage.prototype.postMessage = function(title, channelName, message, icon) {
-    var channelId = this.getChannelId(channelName);
-    this.slackApp.postMessage(channelId,
-                              message, 
-                              {
-                                username : title,
-                                icon_emoji : ":" + icon + ":",
-                              });
-  };
-  
-  SlackMessage.prototype.getChannelId = function(channelName) {
-    var channel = this.channels.filter(function(item, index) {
-      return item.name === channelName;
-    });
-    return channel[0] ? channel[0].id : null;
-  };
-  
-  return SlackMessage;
-})();
-
-
-function loadWorkdays() {
-  var workdays = [];
-  var calendar = CalendarApp.getCalendarById('ja.japanese#holiday@group.v.calendar.google.com');
-  var now = new Date();
-  var dateAfterYear = new Date();
-  dateAfterYear.setYear(dateAfterYear.getFullYear() + 1);
-  var holidays = calendar.getEvents(now, dateAfterYear).map(function(item) {
-    var holidayDate = item.getStartTime();
-    return holidayDate.getFullYear() + "/" + (holidayDate.getMonth() + 1) + "/" + holidayDate.getDate();
-  }); 
-  for (var i = 0; i < 365; i++) {
-    var weekday = now.getDay();
-    var formattedDate = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
-    var isHoliday = holidays.indexOf(formattedDate) != -1;
-    workdays[i] = (weekday == SUNDAY || weekday == SATURDAY || isHoliday) ? 0 : 1;
-    now.setDate(now.getDate() + 1);
-  }
-  return workdays;
-}
-
-
-function countWorkday(days, workdays) {
-  if (days <= 0) {
-    return 0;
-  }
-  var targetWorkdays = workdays.slice(0, days);
-  var count = targetWorkdays.reduce(function(result, item, index, array) {
-    return result + (index < days ? item : 0); 
-  });
-  return count;
 }
